c0d4083f255cb376caf2c6b513a54aff21bf6a76
trung1310 <trung.nguyen@terralogic.com>
2020-07-22 11:35:55 +0700

    Fixing scss...

--- assignment_2_login_form/src/components/Button/Button.js
+++ assignment_2_login_form/src/components/Button/Button.js
@@ -3,6 +3,6 @@ import classes from './button.module.scss'
 
 export default function Button(props) {
     return (
-    <button type="button" className={classes.btnDefault}>{props.name}</button>
+    <button type="button" className={props.btnDefault, classes.btnDefault}>{props.name}</button>
     )
 }
--- assignment_2_login_form/src/containers/Profile/Profile.js
+++ assignment_2_login_form/src/containers/Profile/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './profile.module.scss';
+import './profile.module.scss'
 import Avatar from '../../assets/images/avatar.png'
 import Button from '../../components/Button/Button'
 
@@ -7,60 +8,61 @@ export default function Profile() {
   return (
     <div className='container'>
       <header>
-        <h1 className={classes.title}>My Profile</h1>
-        <h6 className={classes.subtitle}>
+        <h1 className='title'>My Profile</h1>
+        <h6 className='subtitle'>
           Manage your profile and contact information
         </h6>
       </header>
       <section>
-          <div className={classes.profile}>
-              <img className={classes.image} src={Avatar}/>
-              <h3 className={classes.name}>Nguyễn Chí Trung</h3>
+          <div className='profile'>
+              <img className='image' src={Avatar}/>
+              <h3 className='name'>Nguyễn Chí Trung</h3>
           </div>
         <form className='form-group col-md-12'>
           <div className='row mb-2'>
             <div className='form-group col-md-6'>
-              <label className={classes.labelName} className="text-center" for='fullName'>Full Name</label>
+              <label className='labelName' className="text-center" for='fullName'>Full Name</label>
               <input className='form-control' type='text' id='fullName' value='Nguyễn Chí Trung'/>
             </div>
           </div>
 
           <div className='row mb-2'>
             <div className='form-group col-md-6'>
-              <label className={classes.labelEmail} for='email'>Email</label>
+              <label className='labelEmail' for='email'>Email</label>
               <input className='form-control' type='email' id='emailInput' value='trung.nguyen@terralogic.com'/>
             </div>
 
             <div className='form-group col-md-6'>
-              <label className={classes.labelPhone} for='phone'>Phone</label>
+              <label className='labelPhone' for='phone'>Phone</label>
               <input className='form-control' type='text' id='phoneInput' value='111-222-333'/>
             </div>
           </div>
 
-          <div className={classes.line}></div>
-          <p className={classes.changePass}>Change Password</p>
+          <div className='line'></div>
+          <p className='changePass'>Change Password</p>
 
           <div className='row mb-2'>
             <div className='form-group col-md-6'>
-              <label className={classes.labelCurrentPassword} for='currentPassword'>Current Password</label>
+              <label className='labelCurrentPassword' for='currentPassword'>Current Password</label>
               <input className='form-control' type='password' id='currentPasswordInput'/>
             </div>
           </div>
 
           <div className='row mb-2'>
             <div className='form-group col-md-6'>
-              <label className={classes.labelNewPassword} for='newPassword'>New Password</label>
+              <label className='labelNewPassword' for='newPassword'>New Password</label>
               <input className='form-control' type='password' id='newPasswordInput'/>
             </div>
 
             <div className='form-group col-md-6'>
-              <label className={classes.labelConfirmPassword} for='confirmPassword'>Confirm Password</label>
+              <label className='labelConfirmPassword' for='confirmPassword'>Confirm Password</label>
               <input className='form-control' type='password' id='confirmPasswordInput'/>
             </div>
           </div>
 
-          <Button className={classes.btnSave} id='btnSave' name='Save'/>
-          <Button className={classes.btnLogout} id='btnLogout' name='Log out'/>
+          <Button className='btnSave' id='btnSave' name='Save'/>
+          <Button className='btnLogout' id='btnLogout' name='Log out'/>
+          <Button  btnDefault={classes.btnLogout} id='btnLogout' name='Log out'/>
         </form>
       </section>
     </div>
