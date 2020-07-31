2f6809a5cee2eea6ccbd5f4daeb1e9bd3ea5d54f
Nguyễn Chí Trung <nguyenchitrung1310@gmail.com>
2020-07-31 11:39:03 +0700

    update login slice

---
+++ assignment_2_login_form/src/features/login/LoginSlice.ts
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { APP_PROGRESS_STATUS } from "../../constants/index";
+import REDUCER_NAMES from "../reducerNames";
+import { LoginResponseType } from '../../api/types'
+
+const loginSlice = createSlice({
+    name: REDUCER_NAMES.LOGIN,
+    initialState: {
+        loginResponse: {
+            status: APP_PROGRESS_STATUS.IDLE,
+            isLoginSuccess: false,
+            response: {} as LoginResponseType,
+        },
+    },
+    reducers: {
+        loginAttempt: (state) => {
+            state.loginResponse = {
+                status: APP_PROGRESS_STATUS.STARTING,
+                isLoginSuccess: false,
+                response: {} as LoginResponseType,
+            };
+        },
+        loginSuccess: (state, { payload }: { payload: LoginResponseType }) => {
+            state.loginResponse = {
+                status: APP_PROGRESS_STATUS.SUCCESS,
+                isLoginSuccess: true,
+                response: payload,
+            };
+        },
+        loginFail: (state, { payload }: { payload: LoginResponseType }) => {
+            state.loginResponse = {
+                status: APP_PROGRESS_STATUS.FAILED,
+                isLoginSuccess: false,
+                response: payload,
+            };
+        }
+    },
+});
+
+const { actions, reducer } = loginSlice;
+export default reducer;
+export const { loginSuccess, loginFail, loginAttempt } = actions;
+
