8fe5e1fc95f577a0a93eceafad66396f2482bd44
Nguyễn Chí Trung <nguyenchitrung1310@gmail.com>
2020-07-31 11:36:07 +0700

    update path

---
+++ assignment_2_login_form/src/constants/index.ts
@@ -0,0 +1,18 @@
+
+// default path
+export const DEFAULT_PATH = "/";
+// login path
+export const LOGIN_PATH = "/login";
+// register path
+export const REGISTER_PATH = "/register";
+
+
+// ***************************
+// App constants
+export const APP_PROGRESS_STATUS = {
+    IDLE: "IDLE",
+    PENDING: "PENDING",
+    STARTING: "STARTING",
+    SUCCESS: "SUCCESS",
+    FAILED: "FAILED",
+};
