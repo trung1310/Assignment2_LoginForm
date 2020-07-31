a131ac0895ed9ce97fbc84e53d228db221788f6d
Nguyễn Chí Trung <nguyenchitrung1310@gmail.com>
2020-07-31 11:44:58 +0700

    Create and update store reducer

---
+++ assignment_2_login_form/src/store.ts
@@ -0,0 +1,28 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import loginReducer from "./features/login/LoginSlice";
+import registerReducer from "./features/register/RegisterSlice";
+import profileReducer from "./features/profile/ProfileSlice";
+
+const CLEAR_STORE = "CLEAR_STORE";
+
+const appReducer = combineReducers({
+    login: loginReducer,
+    register: registerReducer,
+    profile: profileReducer,
+});// what
+
+const rootReducer = (state: any, action: any) => {
+    if (action.type === CLEAR_STORE) {
+        localStorage.clear();
+        state = undefined;
+    }
+    return appReducer(state, action);
+}; // what exactly
+
+const store = configureStore({
+    reducer: rootReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>; // ???
+export { CLEAR_STORE };
+export default store;
